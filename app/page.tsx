262649bb043f2f8ad9d5c329f29abb86b871f223
Priya Sharma <priyasharma2702@gmail.com>
2025-04-14 23:50:10 +0200

    added some fonts and css module

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Suspense } from "react";
 import LoginForm from "./ui/LoginForm";
-//check
+import styles from '@/app/ui/home.module.css';
+import { lusitana } from "@/app/ui/fonts";
+
 export default function LoginPage(){
     return (
         <main className="flex items-center justify-center md:h-screen">
+            <p className={`${lusitana.className}${styles.shape}`}>Login Page</p>
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <Suspense>
                     <LoginForm/>
