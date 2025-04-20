93d2b442e146671f51908050f2102bc3459d2720
Priya Sharma <priyasharma2702@gmail.com>
2025-04-20 23:56:53 +0200

    added some cutomers images and latest invoices components

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,6 +1,8 @@
 import { LatestInvoice } from '@/app/lib/definitions';
 import { lusitana } from '../fonts';
 import Image from 'next/image';
+import clsx from 'clsx';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
 
 const LatestInvoices = ({
 	latestInvoices,
@@ -12,28 +14,44 @@ const LatestInvoices = ({
 			<h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
 				Latest Invoices
 			</h2>
-			<div className='rounded-xl bg-gray-50 p-4'>
-				<div className='flex justify-between'>
-					{latestInvoices.map((latestInvoice) => (
+			<div className='flex grow flex-col justify-between rounded-xl bg-gray-50 p-4'>
+				<div className='bg-white px-6'>
+					{latestInvoices.map((invoice, i) => (
 						<div
-							key={latestInvoice.id}
-							className='flex justify-between'>
-							<div>
+							key={invoice.id}
+							className={clsx(
+								'flex flex-row items-center justify-between py-4',
+								{
+									'border-t': i !== 0,
+								}
+							)}>
+							<div className='flex items-center'>
 								<Image
-									src={latestInvoice.image_url}
-									alt={latestInvoice.name}
+									src={invoice.image_url}
+									alt={`${invoice.name}'s profile pricture`}
 									className='mr-4 rounded-full'
 									width={32}
 									height={32}
 								/>
-								<div>
-									<div>{latestInvoice.name}</div>
-									<div>{latestInvoice.email}</div>
+								<div className='min-w-0'>
+									<p className='truncate text-sm font-semibold md:text-base'>
+										{invoice.name}
+									</p>
+									<p className='hidden text-sm text-gray-500 sm:block'>
+										{invoice.email}
+									</p>
 								</div>
 							</div>
-							<div>{latestInvoice.amount}</div>
+							<p
+								className={`${lusitana.className} truncate text-sm font-medium md:text-base`}>
+								{invoice.amount}
+							</p>
 						</div>
 					))}
+				</div>
+				<div className='flex items-center pb-2 pt-6'>
+					<ArrowPathIcon className='h-5 w-5 text-gray-500' />
+					<h3 className='ml-2 text-sm text-gray-500'>Updated just now</h3>
 				</div>
 			</div>
 		</div>
