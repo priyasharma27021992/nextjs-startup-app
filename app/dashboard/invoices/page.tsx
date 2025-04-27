0c8c1d507049352a1ff6aa24d94862ee93177713
Priya Sharma <priyasharma2702@gmail.com>
2025-04-27 23:27:58 +0200

    just started adding search params in incvoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,7 +3,16 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { Search } from '@/app/ui/search';
 import React from 'react';
 
-const Invoices = () => {
+const Invoices = async (props: {
+	searchParams?: Promise<{
+		query?: string;
+		page?: string;
+	}>;
+}) => {
+	const searchParams = await props?.searchParams;
+	const query = searchParams?.query || '';
+	const currentPage = Number(searchParams?.page) || 1;
+
 	return (
 		<div className='w-full'>
 			<div className='flex w-full items-center justify-between'>
